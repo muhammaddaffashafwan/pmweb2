07ae589158b3f4e7a9b558d6a8be44fb96d041cf
muhammaddaffashafwan <114461495+muhammaddaffashafwan@users.noreply.github.com>
2023-03-11 14:13:41 +0700

    praktikum 3

---
+++ praktikum 3/for_looping.php
@@ -0,0 +1,5 @@
+<?php
+for($nomor = 0 ;$nomor < 10;$nomor++){
+    echo "Hello TI11 , ini looping ke-$nomor <br>";
+}
+?>
---
+++ praktikum 3/while_looping.php
@@ -0,0 +1,7 @@
+<?php
+$nomor = 0;
+while($nomor != 10){
+    echo "ini looping ke-$nomor <br>";
+    $nomor++;
+}
+?>
